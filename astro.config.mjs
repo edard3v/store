2cc9cbd291f90efdd876971e327f425ed92ca458
edard3v <edard3v@gmail.com>
2024-12-25 08:24:09 -0500

    borrando la mrd de authjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,6 @@ import cloudflare from "@astrojs/cloudflare";
 
 import db from "@astrojs/db";
 
-import auth from "auth-astro";
-
 // https://astro.build/config
 export default defineConfig({
   vite: {
@@ -16,5 +14,5 @@ export default defineConfig({
   },
   output: "static",
   adapter: cloudflare(),
-  integrations: [db(), auth()],
+  integrations: [db()],
 });

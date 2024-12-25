2550005daa90f5c417c07757983fb1147651c65f
edard3v <edard3v@gmail.com>
2024-12-25 08:22:07 -0500

    refactor

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,11 @@ import auth from "auth-astro";
 
 // https://astro.build/config
 export default defineConfig({
+  vite: {
+    build: {
+      assetsInlineLimit: 0,
+    },
+  },
   output: "static",
   adapter: cloudflare(),
   integrations: [db(), auth()],

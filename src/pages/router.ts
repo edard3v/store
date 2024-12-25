952d264d1f058256c5ffd648f172e8a69cb1f46a
edard3v <edard3v@gmail.com>
2024-12-25 03:00:13 -0500

    REFACTor

--- src/pages/router.ts
+++ src/pages/router.ts
@@ -3,9 +3,18 @@ export const ROUTER = {
     href: "/home/",
     display: "Inicio",
   },
-
   contact: {
     href: "/contact/",
     display: "Cotacto",
   },
+  login: {
+    href: "/login/",
+    display: "Login",
+  },
+  register: {
+    href: "/register/",
+    display: "Registro",
+  },
 };
+
+export const PAGES = Object.entries(ROUTER).map(([_, value]) => value);

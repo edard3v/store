81cf71394550f82614423857d8db292d7a13f39b
edard3v <edard3v@gmail.com>
2024-12-26 05:42:48 -0500

    auth funcional

--- src/actions/auth/login/login.action.ts
+++ src/actions/auth/login/login.action.ts
@@ -8,7 +8,18 @@ export const login = defineAction({
     email: z.string().email(),
     password: z.string().min(6),
   }),
-  async handler(form) {
-    return await loginService(form);
+  async handler(form, { cookies }) {
+    const { token } = await loginService(form);
+
+    // Guardar el token en una cookie segura
+    cookies.set("token", token, {
+      httpOnly: true, // Impide el acceso a la cookie desde JavaScript
+      secure: true, // Solo se envía por conexiones HTTPS
+      sameSite: "strict", // Previene el envío de cookies en solicitudes entre sitios
+      maxAge: 7 * 24 * 60 * 60, // expira en 1 semana
+      path: "/", // La cookie estará disponible para todo el dominio
+    });
+
+    return { success: true };
   },
 });
--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -1,9 +1,14 @@
 import { defineMiddleware } from "astro:middleware";
+import { ROUTER } from "src/pages/router";
 
 export const onRequest = defineMiddleware((context, next) => {
-  console.log("middle");
-  const isAuth = false;
-  context.locals.isAuth = isAuth;
+  console.log("middleware");
+
+  const { url, cookies, locals } = context;
+
+  if (url.pathname == ROUTER.account.href) {
+    locals.isAuth = !!cookies.get("token")?.value;
+  }
 
   return next();
 });

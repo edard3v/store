75cfe946bdf9ece3386c621476bb985442545a80
edard3v <edard3v@gmail.com>
2024-12-26 21:50:21 -0500

    refactor

--- src/jwt/isTokenOld.ts
+++ src/jwt/isTokenOld.ts
@@ -6,7 +6,7 @@ export const isTokenOld = (token: string): boolean => {
   if (!payload?.iat) return true;
 
   const currentTime = Math.floor(Date.now() / 1000); // Tiempo actual en segundos
-  const days = 15; // 3 días en segundos 3 * 24 * 60 * 60
+  const days = 3 * 24 * 60 * 60; // 3 días en segundos
 
   return currentTime - payload.iat > days;
 };
